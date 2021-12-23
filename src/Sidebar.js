5d0035a3ce067aa742371b245f2b58b69ec7bff8
Deepanshu Singh Negi <88818854+DiegoDialga@users.noreply.github.com>
2021-12-23 20:52:58 +0530

    Add files via upload

---
+++ src/Sidebar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Sidebar.css';
+import SidebarOption from './SidebarOption.js';
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from '@material-ui/icons/Search';
+import LibraryMusicIcon from '@material-ui/icons/LibraryMusic';
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <img 
+            className='sidebar_logo'
+            src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt=""/>
+            <SidebarOption Icon={HomeIcon} title="Home"/>
+            <SidebarOption title="Search" />
+            <SidebarOption title="Your Library" />
+
+
+        </div>
+    )
+};
+
+export default Sidebar;
